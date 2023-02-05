633bd16e57107560d5aacc1179e3954d4c38441e
djohnson518 <122762485+djohnson518@users.noreply.github.com>
2023-02-05 16:47:26 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,42 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>HTML1</title>
+</head>
+<h1 style="color:darkred">The Red-Spot Planet</h1>
+<body>
+&nbsp;No Moons&nbsp;No Mo&nbsp;No Mor&nbsp;92 Moons&nbsp;9&nbsp;1 Moon&nbsp;1&nbsp;No Life&nbsp;No Life&nbsp;Life&nbsp;Sun&nbsp;Jupiter&nbsp;Earth<img src="jupiterjpg.jpg" width="192" height="240" alt=""/>
+</body>
+<p>Jupiter is a reddish colored planet. Due to its size, it is categorized as a jovian planet. This means that it is <strong>gigantic</strong> compared to Earth. It has an equatorial diameter of about 88,900 miles. </p>
+<h2>Jupiter Compared to Earth </h2>
+	<li>Circumference is 11x larger than Earth</li>
+	<li>Temperature is 4x cooler than Earth</li>
+	<li>Rotation is 2.5x faster than Earth</li>
+<img src="solarsystemscale.jpg" width="302" height="160" alt=""/>
+<h3>Life On Jupiter</h3>
+<p>There are stripes and swirls of cold and windy clouds of ammonia and water. The atmosphere is made up of helium and hydrogen. Jupiter cannot support life, however, some of Jupiter's moons have oceans under the crusts that could support life.</p>
+<table width="200" border="1">
+  <tbody>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+  </tbody>
+</table>
+
+<img src="jupiterclouds.jpg" width="420" height="157" alt=""/>
+<a href="url">https://solarsystem.nasa.gov/planets/jupiter/overview/</a>
+</html>
+	
